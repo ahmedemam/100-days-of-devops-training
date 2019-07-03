132c5ff2d7509e0897f96f3f7187865a9b090a91
ahmedemam <zizoahmed980@gmail.com>
2019-07-03 12:49:22 +0200

    docker course: assignment 1

---
+++ docker/assiegment_1.sh
@@ -0,0 +1,18 @@
+# Assignment #1: managing multiple containers 
+    # -- mysql / nginx / httpd 
+
+# remove images 
+
+# running mysql 
+docker search mysql
+docker run --name=mysql -p 3306:3306 --env MYSQL_ROOT_RANDOM_PASSWORD=yes -d mysql
+docker run --name=mysql -p 3306:3306 --env MYSQL_USER=root MYSQL_USER_PASSWORD=password -d mysql
+
+# running httpd
+docker search httpd 
+docker run --name=httpd -p 8080:80 -d httpd
+
+# running nginx 
+docker search nginx 
+docker run --name=nginx -p 80:80 -d nginx
+
